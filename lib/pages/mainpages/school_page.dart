eb5f9c9c9da0e95ad37b950ae67370dd13d1a512
swoo0105 <andyswooseo@gmail.com>
2023-09-17 12:21:56 +0900

    Korea_highschool_community_app_project

--- lib/pages/mainpages/school_page.dart
+++ lib/pages/mainpages/school_page.dart
@@ -1,8 +1,8 @@
-import 'dart:ffi';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../../utility/utils.dart';
 
 class SchoolPage extends StatefulWidget {
   const SchoolPage({super.key});
@@ -12,46 +12,47 @@ class SchoolPage extends StatefulWidget {
 }
 
 class _SchoolPageState extends State<SchoolPage> {
-  String? mySchool;
-
-  Future _getSchoolName() async {
-    String currentUserEmail =
-    FirebaseAuth.instance.currentUser!.email.toString();
-    await FirebaseFirestore.instance
+  Future<DocumentSnapshot> getDocument() async {
+    var firebaseUser =
+        await FirebaseAuth.instance.currentUser!.email.toString();
+    return FirebaseFirestore.instance
         .collection("users")
-        .doc(currentUserEmail)
-        .get()
-        .then((snapshot) async {
-      if (snapshot.exists) {
-        setState(() {
-          mySchool = snapshot.data()!["schoolvalue"].toString();
-          print(mySchool);
-        });
-      }
-    });
+        .doc(firebaseUser)
+        .get();
   }
 
   @override
-  void initState() {
-    super.initState();
-    _getSchoolName().then((schoolName) {
-      setState(() {
-        mySchool = schoolName;
-      });
-    });
-  }
-
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-              ],
-            ),
+        body: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  FutureBuilder(
+                      future: getDocument(),
+                      builder:
+                          (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          return Text(
+                            snapshot.data!["schoolvalue"].toString(),
+                            style: SafeGoogleFont(
+                              'Noto Sans KR',
+                              fontSize: 30,
+                              fontWeight: FontWeight.w100,
+                            ),
+                          );
+                        } else if (snapshot.connectionState ==
+                            ConnectionState.none) {
+                          return Text("error ??");
+                        }
+                        return CircularProgressIndicator();
+                      })
+                ],
+              )
+            ],
           ),
         ),
       ),
