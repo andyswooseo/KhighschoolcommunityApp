05faf25d81b9f42a48d686c8307895a7df1cd8f8
swoo0105 <andyswooseo@gmail.com>
2023-09-22 01:54:32 +0900

    Korea_highschool_community_app_project

--- lib/pages/mainpages/school_page.dart
+++ lib/pages/mainpages/school_page.dart
@@ -1,8 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_neumorphic/flutter_neumorphic.dart';
-
 import '../../utility/utils.dart';
 
 class SchoolPage extends StatefulWidget {
@@ -26,10 +24,37 @@ class _SchoolPageState extends State<SchoolPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Color(0xFFE7E7E7),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.indigo,
+          elevation: 0,
+          title: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Text(
+                  '우리 학교',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontFamily: 'BMHANNA', fontSize: 30),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  child: Text(
+                    '학교 커뮤니티',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontFamily: 'BMHANNA', fontSize: 30)
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
         body: Padding(
           padding: const EdgeInsets.all(17),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
                 padding: const EdgeInsets.only(bottom: 50),
@@ -47,11 +72,8 @@ class _SchoolPageState extends State<SchoolPage> {
                                   ConnectionState.done) {
                                 return Text(
                                   snapshot.data!["schoolvalue"].toString(),
-                                  style: SafeGoogleFont(
-                                    'Noto Sans KR',
-                                    fontSize: 25,
-                                    fontWeight: FontWeight.w900,
-                                  ),
+                                  style: TextStyle(
+                                      fontFamily: 'BMHANNA', fontSize: 40),
                                 );
                               } else if (snapshot.connectionState ==
                                   ConnectionState.none) {
@@ -59,13 +81,15 @@ class _SchoolPageState extends State<SchoolPage> {
                               }
                               return CircularProgressIndicator();
                             }),
+                        SizedBox(
+                          height: 5,
+                        ),
                         Text(
                           '2-3 박범진 학생',
-                          style: SafeGoogleFont(
-                            'Noto Sans KR',
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                          ),
+                          style: TextStyle(
+                              fontFamily: 'BMHANNA',
+                              fontSize: 20,
+                              fontWeight: FontWeight.w200),
                         ),
                       ],
                     ),
@@ -108,20 +132,35 @@ class _SchoolPageState extends State<SchoolPage> {
                   ],
                 ),
               ),
-              Row(
-                children: [
-                  Neumorphic(
-                    style: NeumorphicStyle(
-                      depth: -10,
-                      shadowLightColor: Colors.white
-                    ),
-                    child: Expanded(
+              Text(
+                '급식 일정표',
+                style: TextStyle(fontFamily: 'BMHANNA', fontSize: 20),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              SingleChildScrollView(
+                child: Row(
+                  children: [
+                    Expanded(
                       flex: 1,
                       child: Container(
                         decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey[500]!,
+                                offset: const Offset(2, 2),
+                                blurRadius: 15,
+                                spreadRadius: 1,
+                              ),
+                              const BoxShadow(
+                                  color: Colors.white,
+                                  offset: Offset(-2, -2),
+                                  blurRadius: 15,
+                                  spreadRadius: 1)
+                            ]),
                         child: Padding(
                           padding: const EdgeInsets.all(15),
                           child: Column(
@@ -151,39 +190,192 @@ class _SchoolPageState extends State<SchoolPage> {
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Container(
-                      decoration: BoxDecoration(color: Colors.white),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '오늘의 할 일을 완성해주세요.',
-                              style: SafeGoogleFont(
-                                'Noto Sans KR',
-                                fontSize: 15,
-                                fontWeight: FontWeight.w600,
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey[500]!,
+                                offset: const Offset(2, 2),
+                                blurRadius: 15,
+                                spreadRadius: 1,
                               ),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text('1. 모의고사 문제 10개 풀기'),
-                            Text('2. 영어 단어 40개 외우기'),
-                            Text('3. 한국사 페이지 10~40 페이지 외우기'),
-                          ],
+                              const BoxShadow(
+                                  color: Colors.white,
+                                  offset: Offset(-2, -2),
+                                  blurRadius: 15,
+                                  spreadRadius: 1)
+                            ]),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Text(
+                                '오늘의 급식 ??',
+                                style: SafeGoogleFont(
+                                  'Noto Sans KR',
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                '현미밥 \n맛있는 김 \n김치 \n호두과자 \n야자열매',
+                                style: SafeGoogleFont(
+                                  'Noto Sans KR',
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w300,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey[500]!,
+                                offset: const Offset(2, 2),
+                                blurRadius: 15,
+                                spreadRadius: 1,
+                              ),
+                              const BoxShadow(
+                                  color: Colors.white,
+                                  offset: Offset(-2, -2),
+                                  blurRadius: 15,
+                                  spreadRadius: 1)
+                            ]),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Text(
+                                '오늘의 급식 ??',
+                                style: SafeGoogleFont(
+                                  'Noto Sans KR',
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                '현미밥 \n맛있는 김 \n김치 \n호두과자 \n야자열매',
+                                style: SafeGoogleFont(
+                                  'Noto Sans KR',
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w300,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey[500]!,
+                                offset: const Offset(2, 2),
+                                blurRadius: 15,
+                                spreadRadius: 1,
+                              ),
+                              const BoxShadow(
+                                  color: Colors.white,
+                                  offset: Offset(-2, -2),
+                                  blurRadius: 15,
+                                  spreadRadius: 1)
+                            ]),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Text(
+                                '오늘의 급식 ??',
+                                style: SafeGoogleFont(
+                                  'Noto Sans KR',
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                '현미밥 \n맛있는 김 \n김치 \n호두과자 \n야자열매',
+                                style: SafeGoogleFont(
+                                  'Noto Sans KR',
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w300,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                decoration: BoxDecoration(color: Colors.white),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '오늘의 할 일을 완성해주세요.',
+                        style: SafeGoogleFont(
+                          'Noto Sans KR',
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text('1. 모의고사 문제 10개 풀기'),
+                      Text('2. 영어 단어 40개 외우기'),
+                      Text('3. 한국사 페이지 10~40 페이지 외우기'),
+                    ],
                   ),
-                ],
+                ),
               ),
             ],
           ),
